9a6a3ef2409fd23b8d5a617e0950c8d2e0c744d1
wiseqx <wangs.ashley@gmail.com>
2020-02-16 12:01:04 -0600

    Added my main code file

---
+++ challenge.py
@@ -0,0 +1,46 @@
+import csv
+
+row_count=0
+candidates_list=[]
+vote_dict={}
+with open('Unit3 - Python_Homework_PyPoll_Resources_election_data.csv','r') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    next(csv_reader,None)
+    for row in csv_reader:
+        row_count+=1
+        #Count the total number of votes each candidate won
+        if row[2] not in candidates_list:
+            candidates_list.append(row[2])
+            vote_dict[str(row[2])]=[1]
+        else:
+            vote_dict[str(row[2])][0]+=1
+
+    #Sort the winner of the election based on popular vote
+    num_of_votes=0
+    for key in vote_dict.keys():
+        if int(vote_dict[key][0]) > num_of_votes:
+            num_of_votes=int(vote_dict[key][0])
+            winner_name=key
+
+    #Count the percentage of votes each candidate won
+    total_vote=sum([i[0] for i in vote_dict.values()])
+    for name in candidates_list:
+        if name in vote_dict:
+            percentage_of_vote='{:.3%}'.format(float(vote_dict[str(name)][0]/total_vote))
+            vote_dict[str(name)].append(percentage_of_vote)
+
+
+    with open("PyPoll_result.txt","w") as output_file:
+        #first two lines
+        title='Election Results \n'+"-"*30+"\n"+"Total Votes: "+str(total_vote)+"\n"+"-"*30+"\n"
+        print(title)
+        output_file.write(title)
+        for name in candidates_list:
+            #candidate info
+            candidate_info=name+": "+str(vote_dict[str(name)][1])+" ("+str(vote_dict[str(name)][0])+")"+"\n"
+            print(candidate_info)
+            output_file.write(candidate_info)
+        #winner info
+        Winner="-"*30+"\n"+"Winner: "+winner_name+"\n"+"-"*30
+        print(Winner)
+        output_file.write(Winner)
---
+++ main.py
@@ -0,0 +1,54 @@
+import csv
+
+months=[]
+total_amount=0
+max_amount=0
+min_amount=0
+profit_change=[]
+
+
+with open('Unit3 - Python_Homework_PyBank_Resources_budget_data.csv','r') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    #skip header
+    next(csv_reader,None)
+    csv_list=list(csv_reader)
+
+    #The total number of months included in the dataset
+    total_month=len(csv_list)
+
+    previous_profit_num=int(csv_list[0][1])
+    for row in range(len(csv_list)):
+        #The net total amount of "Profit/Losses" over the entire period
+        total_amount+=int(csv_list[row][1])
+
+        #Calculate every change in "Profit/Losses" over the entire period
+        net_change=int(csv_list[row][1])-previous_profit_num
+        profit_change.append(net_change)
+        previous_profit_num=int(csv_list[row][1])
+        
+    #calculate the average of the changes in "Profit/Losses" over the entire period
+    average_change=round(sum(profit_change)/(len(csv_list)-1),2)
+
+    #The greatest increase in profits (date and amount) over the entire period
+    for i in range(len(profit_change)):
+        if int(profit_change[i])>max_amount:
+            max_index_number=i
+            max_amount=int(profit_change[i])
+        elif int(profit_change[i])<min_amount:
+            min_index_number=i
+            min_amount=int(profit_change[i])
+    
+    #print summary table
+    Summary_table="Financial Analysis \n" \
+    +str("-"*30)+"\n" \
+    +"Total Months: "+str(total_month) +"\n" \
+    +"Total: $"+str(total_amount) +"\n" \
+    +"Average  Change: $" +str(average_change) +"\n" \
+    +"Greatest Increase in Profits: "+str(csv_list[max_index_number][0])+ " ($"+str(profit_change[max_index_number])+")\n" \
+    +"Greatest Decrease in Profits: "+str(csv_list[min_index_number][0])+" ($"+str(profit_change[min_index_number])+")"
+    print(Summary_table)
+
+#write to a text file
+output_file = open("PyBank_result.txt","w") 
+output_file.write(Summary_table)
+output_file.close()
